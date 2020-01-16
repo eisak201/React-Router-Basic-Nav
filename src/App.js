7e197ae78b100199c6dce7d7371688246c5dcd0a
Joe <leftnovbrown@gmail.com>
2020-01-15 21:31:47 -0500

    started Router I exercise. Ading components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Route, BrowserRouter} from 'react-router-dom'
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+
+
 const App = () => (
   <div>
-    <Navigation />
-  </div>
+    <Navigation /> 
+
+<BrowserRouter>
+<Route exact path='/' components={Home} />
+<Route path='/' components={About} />
+<Route path='/' components={Contact} />
+</BrowserRouter>
+</div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<BrowserRouter><App /></BrowserRouter >, document.getElementById('root'));
